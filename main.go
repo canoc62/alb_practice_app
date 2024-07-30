62bec62af03cccca927ba30f8f5272c9ca2b6161
Christopher Cano <canoc4262@gmail.com>
2024-07-30 01:04:39 -0700

    Temp change traffic port back to 8080.

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ func healthRouter() http.Handler {
 
 func main() {
 	mainServer := &http.Server{
-		Addr:         ":80",
+		Addr:         ":8080",
 		Handler:      mainRouter(),
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
